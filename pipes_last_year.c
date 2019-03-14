e468dabbb2b34141be498f509047453b2ab78034
Damian Roszczyk <34535631+buensons@users.noreply.github.com>
2019-03-14 11:39:47 +0100

    Update pipes_last_year.c

--- pipes_last_year.c
+++ pipes_last_year.c
@@ -13,25 +13,20 @@
 void child_work(int * fd_1, int * fd_2)
 {
     srand(getpid());
-    int ret, res, index;
+    int ret;
+    int value;
     do {
-        char buff[16];
-        if((ret = read(fd_1[0], buff, strlen(buff))) < 0) ERR("read");
-        else if(ret == 0) return;
-        else {
-            index = rand() % strlen(buff);
-            char new_string[ret];
-            int j = 0;
-            for(int i = 0; i < strlen(buff); i++) {
-                if(i != index) {
-                    new_string[j] = buff[i];
-                    j++;
-                }
-            }
-            printf("CHILD PID: %d | %s\n", getpid(), new_string);
-            if((res = write(fd_2[1], new_string, ret)) < ret) ERR("write");
+        if((ret = read(fd_1[0], &value, sizeof(value))) < 0) ERR("read");
+        else if(ret == 0) {
+            printf("child ret is 0\n");
+            return;
         }
-    } while(ret > 0);
+        else {
+            if(value == 0) return;
+            printf("CHILD PID: %d | %d\n", getpid(), --value);
+            if(write(fd_2[1], &value, sizeof(value)) < sizeof(value)) ERR("write");
+        }
+    } while(value > 0);
     close(fd_1[0]);
     close(fd_2[1]);
 }
@@ -39,26 +34,20 @@ void child_work(int * fd_1, int * fd_2)
 void parent_work(int * fd_1, int * fd_2)
 {
     srand(getpid());
-    int ret, res, index;
+    int ret;
+    int value;
     do {
-        char buff[16];
-        if((ret = read(fd_2[0], buff, strlen(buff))) < 0) ERR("read");
-        else if(ret == 0) return;
-        else {
-            index = rand() % strlen(buff);
-            char new_string[ret];
-            int j = 0;
-            for(int i = 0; i < strlen(buff) + 1; i++) {
-                if(i != index) {
-                    new_string[j] = buff[i];
-                    j++;
-                }
-            }
-            printf("PARENT PID: %d | %s\n", getpid(), new_string);
-            
-            if((res = write(fd_1[1], new_string, ret)) < ret) ERR("write");
+        if((ret = read(fd_2[0], &value, sizeof(value))) < 0) ERR("read");
+        else if(ret == 0) {
+            printf("parent ret is 0\n");
+            return;
         }
-    } while(ret > 0);
+        else {
+            if(value == 0) return;
+            printf("PARENT PID: %d | %d\n", getpid(), --value);
+            if(write(fd_1[1], &value, sizeof(value)) < sizeof(value)) ERR("write");
+        }
+    } while(value > 0);
 }
 
 int main(int argc, char ** argv)
@@ -66,11 +55,10 @@ int main(int argc, char ** argv)
     int fd_1[2];
     int fd_2[2];
     int ret;
-    char * string;
 
     if(argc != 2) ERR("usage");
+    int value = atoi(argv[1]);
 
-    string = argv[1];
     if((ret = pipe(fd_1)) < 0) ERR("pipe");
     if((ret = pipe(fd_2)) < 0) ERR("pipe");
     if((ret = fork()) < 0) ERR("fork");
@@ -79,11 +67,12 @@ int main(int argc, char ** argv)
         close(fd_1[1]);
         close(fd_2[0]);
         child_work(fd_1, fd_2);
+        exit(0);
     } else {
         //parent
         close(fd_1[0]);
         close(fd_2[1]);
-        if((ret = write(fd_1[1], string, strlen(string))) < strlen(string))
+        if((ret = write(fd_1[1], &value, sizeof(value))) < sizeof(value))
             ERR("write");
 
         parent_work(fd_1, fd_2);
